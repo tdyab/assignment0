ab7b0769271f0e285c3e49516b84273adc9bf6b0
tdyab <taladyab@gmail.com>
2025-09-08 05:35:53 +0300

    adding existing project

---
+++ app/src/main/java/com/example/assignment0/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.assignment0;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView asst0txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        asst0txt = findViewById(R.id.idasst0);
+        asst0txt.setText("assignment by tala diab");
+    }
+}
